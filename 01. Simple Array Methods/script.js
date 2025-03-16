752be5ea8f6b0ca14ac623a50a4e1f6fd7f4f1df
ephraim <ephraims.818@gmail.com>
2025-03-16 19:58:14 +0600

    JS--WORKING-WITH-ARRAYS - 02. The New at Method

--- 01. Simple Array Methods/script.js
+++ 01. Simple Array Methods/script.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// LECTURES
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // Slice method
